a0a35a69f9ebcd1a4f889bb048b4062033e630cf
Elva Eglite <eglite.elva@gmail.com>
2020-08-17 20:54:56 +0300

    Created Xmas tree

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "Scala HW 1808"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
---
+++ src/main/scala/Tree.scala
@@ -0,0 +1,3 @@
+object Tree extends App {
+  Array (" "*3 + "*"," "*2 + "*"*3," "*1 + "*"*5).foreach(item => println(s"$item"))
+}
