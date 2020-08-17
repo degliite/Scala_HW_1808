c27d4f14adef2c7f72eac932affef15f3a757e6c
Elva Eglite <eglite.elva@gmail.com>
2020-08-17 23:02:08 +0300

    Better solution

--- src/main/scala/Tree.scala
+++ src/main/scala/Tree.scala
@@ -1,3 +1,20 @@
 object Tree extends App {
-  Array (" "*3 + "*"," "*2 + "*"*3," "*1 + "*"*5).foreach(item => println(s"$item"))
+  Array(" " * 3 + "*", " " * 2 + "*" * 3, " " * 1 + "*" * 5).foreach(item => println(s"$item"))
+
+  println(" ")
+
+  for (i <- 0 until 3) {
+    for (j <- 0 until 3 - i) {
+      print (" ")
+    }
+    for (z <- 0 until (i * 2 + 1)){
+      print("*")
+    }
+    println()
+  }
+
+
 }
+
+
+
